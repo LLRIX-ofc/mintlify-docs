a7adcfe3d7c39694ebb428a119bd55bd371c699d
LLRIX-ofc <llrix.ofc@gmail.com>
2025-08-14 19:47:28 +0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,10 +1,8 @@
-<script>
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5615292244322800";
-    script.crossOrigin = "anonymous";
-    script.onload = () => {
-        console.log("Custom.js is loaded!");
-    };
-    document.head.appendChild(script);
-</script>
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5615292244322800";
+script.crossOrigin = "anonymous";
+script.onload = () => {
+    console.log("Custom.js is loaded!");
+};
+document.head.appendChild(script);
